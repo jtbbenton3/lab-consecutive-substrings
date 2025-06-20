42986e73d990e60138c7c08bccc68f172fc23d0a
Jack Benton <jtb.benton@gmail.com>
2025-06-20 10:06:15 -0400

    Lab 7

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,18 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+
+  for (let start = 0; start < string.length; start++) {
+    let substring = '';
+    for (let end = start; end < string.length; end++) {
+      substring += string[end];
+      result.push(substring);
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
@@ -14,5 +23,3 @@ if (require.main === module) {
 }
 
 module.exports = consecutiveSubstrings;
-
-
